a4dc649aeb04975fd522455cd56570e863f89bd2
sollendy <edlannino@tutanota.com>
2023-02-22 16:13:55 +0100

    feat: inserito calcolo prezzo totale. problemi con prompt

--- JS/script.js
+++ JS/script.js
@@ -1,9 +1,13 @@
 let età = parseInt(prompt("Inserisci età"));
 let distanza = parseInt(prompt("quale distanza vuoi percorrere?"));
 let prezzo = distanza * 0.21;
+let sconto;
 let totale;
 if( età < 18 ) {
-    totale = (prezzo / 100) * 20;
+    sconto = (prezzo / 100) * 20;
+    totale = prezzo - sconto;
+} else(età > 65) {
+    
 }
 
 document.writeln(totale);

25a75c21cfa17d3c1efd28b22242d5ce36c0589e
sollendy <edlannino@tutanota.com>
2023-02-22 15:59:14 +0100

    primo sconto ultimato

--- JS/script.js
+++ JS/script.js
@@ -1,8 +1,9 @@
 let età = parseInt(prompt("Inserisci età"));
 let distanza = parseInt(prompt("quale distanza vuoi percorrere?"));
 let prezzo = distanza * 0.21;
+let totale;
 if( età < 18 ) {
-    prezzo = ( distanza * 0.21 - 20% );
+    totale = (prezzo / 100) * 20;
 }
 
-document.writeln(prezzo);
+document.writeln(totale);

93c16d85b478e42965dd726947372d3bb2412177
sollendy <edlannino@tutanota.com>
2023-02-22 15:37:33 +0100

    problemi col calcolo sconto

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,8 @@
 let età = parseInt(prompt("Inserisci età"));
 let distanza = parseInt(prompt("quale distanza vuoi percorrere?"));
 let prezzo = distanza * 0.21;
+if( età < 18 ) {
+    prezzo = ( distanza * 0.21 - 20% );
+}
+
 document.writeln(prezzo);
